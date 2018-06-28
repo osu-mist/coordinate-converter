e757914e76a4f3cf2b77e71ba6edcefc4771b973
George Crary <craryg@onid.oregonstate.edu>
2018-06-28 15:24:36 -0700

    Add bulk of the diffing work

--- coord-differ.py
+++ coord-differ.py
@@ -9,10 +9,10 @@
 
 import sys
 import json
-import dictdiffer
+from dictdiffer import diff
 
 from docopt import docopt
-print(docopt(__doc__, version='1.0.0rc2'))
+#print(docopt(__doc__, version='1.0.0rc2'))
 
 args = docopt(__doc__, version='1.0.0rc2')
 
@@ -33,10 +33,12 @@ if __name__ == "__main__":
 
         #if (old_coords_view ^ new_coords_view) != set([]):
         if (old_coords == new_coords):
-            print "There are no differences"
+            #print "There are no differences"
             sys.exit(0)
         else:
-            print "There are differences"
+            #print "There are differences"
+            if(old_coords['crs'] != new_coords['crs']):
+                print "Spacial reference has changed from: {} to {}".format(old_coords['crs'], new_coords['crs'])
 
             old_mapped = mapFeatToID(old_coords)
             new_mapped = mapFeatToID(new_coords)
@@ -58,28 +60,40 @@ if __name__ == "__main__":
                 if(old_mapped[common_feature_id] != new_mapped[common_feature_id]):
                     old = old_mapped[common_feature_id]
                     new = new_mapped[common_feature_id]
-                    print "Start diffing"
+                    print "Start diffing\n"
 
-                    if(old['properties']['AIMS_Name'] != new['properties']['AIMS_Name'] or
+                    try:
+                        #TODO figure out how to handle missing keys for AIMS_Name
+                        if(#old['properties']['AIMS_Name'] != new['properties']['AIMS_Name'] or
                         old['properties']['AiM_Desc'] != new['properties']['AiM_Desc'] or
                         old['properties']['Notes'] != new['properties']['Notes']):
-                        
-                        print "!!! Feature id {}'s name, desc, notes have changed!".format(common_feature_id)
-                        print "OLD {} - {} - {}".format(old['properties']['AIMS_Name'],
-                                                        old['properties']['AiM_Desc'],
-                                                        old['properties']['Notes'])
+                            print "!!! Feature id {}'s desc and notes have changed!".format(common_feature_id)
+                            print "OLD {} - {}".format(#old['properties']['AIMS_Name'],
+                                                            old['properties']['AiM_Desc'],
+                                                            old['properties']['Notes'])
 
-                        print "NEW {} - {} - {}".format(new['properties']['AIMS_Name'],
-                                                        new['properties']['AiM_Desc'],
-                                                        new['properties']['Notes'])
-
+                            print "NEW {} - {}".format(#new['properties']['AIMS_Name'],
+                                                            new['properties']['AiM_Desc'],
+                                                            new['properties']['Notes'])
+                    except KeyError as e:
+                        print "Key Error caught"
+                        pass
                     else:
-                        print "Location id: {} - {} - {} - {}".format(common_feature_id,
-                            old['properties']['AIMS_Name'], old['properties']['AiM_Desc'], old['properties']['Notes'])
+                        print "Location id: {} - {} - {}".format(common_feature_id,
+                                                        old['properties']['AiM_Desc'], old['properties']['Notes'])
 
-                    print "Diffing fields"
+                    print "\nDiffing fields"
                     #dictdiff geometry and properties dict
-                    if(old['geometry']['type'] != new['geometry']['type']):
-                        print "Geometry Type has changed."
+
+                    #diff returns an iterator...
+                    geo_diff = list(diff(old['geometry'], new['geometry']))
+                    properties_diff = list(diff(old['properties'], new['properties']))
+
+                    print "Properties Differences"
+                    print properties_diff
+                    print "\nGeometry Differences"
+                    print geo_diff
+
+                    import pdb; pdb.set_trace()
 
 
