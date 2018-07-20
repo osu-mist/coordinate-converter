188eece6f268c014834f046b5c1149dbff30d77e
George Crary <craryg@onid.oregonstate.edu>
2018-07-20 13:54:57 -0700

    Add initial Docker osgeo/proj image integration test

---
+++ osgeoproj_test.sh
@@ -0,0 +1,7 @@
+export URL='https://services1.arcgis.com/dePSdaG71BplHMCO/arcgis/rest/services/TaPS_Prkg_Zones/FeatureServer/0/query/?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
+
+if docker-compose build --pull --no-cache && docker-compose up ; then
+	exit 0
+else
+	exit 1
+fi;
